3872ad0f404e0e2daf7d61e413a93773138a7563
madhura <iammadhura26@gmail.com>
2023-05-23 20:24:11 +0530

    hi

--- resources/lambda.cjs
+++ resources/lambda.cjs
@@ -1,20 +1,53 @@
-const { data } = require('aws-cdk/lib/logging');
 const AWS = require('aws-sdk');
-const S3 = new AWS.S3();
+const { findConfigFile } = require('typescript');
+const s3 = new AWS.S3();
 
-const bucketName = process.env.BUCKET;
+//const bucketName = process.env.BUCKET;
 
 
-async function main (event, context) {
-  try {
+async function main (bucketName,event, context) {
+  // let connection= await postgree(this);
+  // let contrantracs = await findConntract(connection);
+
+
+
       console.log("Entering Lambda!")
-      const data = await S3.listObjectsV2({ Bucket: bucketName }).promise();
+      const data = await new Promise((resolve, reject) => {s3.listObjectsV2({ Bucket: bucketName },
+        function(err,data){
+          if(err) {console.log(err,err.stack);
+          reject(err)}
+          else{
+            console.log("something",data.Contents);
+            clifiles=JSON.parse(JSON.stringify(data.Contents))
+            console.log(clifiles)
+
+            resolve(data);
+          };
+        })});
       console.log('after s3')
       console.log(data)
-      console.log(data)
-      return data
-  } catch(error) {
-    return data
+      let param = { Bucket: 'cdklambdastack-mylambdastore367fee0e-o3e9kvmjvpgy', Key: 'Pay Slip_100077690_Apr-23.pdf' }
+      const data3= await deleteparam(param)
+      console.log(data3)
+      console.log('after deleting')
+      return 'hello'
   }
+
+async function deleteparam(params){
+  console.log('inside function')
+  await new Promise((resolve, reject) => {
+    s3.deleteObject({ Bucket: 'cdklambdastack-mylambdastore367fee0e-o3e9kvmjvpgy', Key: 'Pay Slip_100077690_Apr-23.pdf' }, function(err, data) {
+      if (err) {
+        console.log('error occured')
+       reject(err);
+      } else {
+        console.log('resolved error')
+        console.log(JSON.stringify(data));
+       resolve(data);
+      }
+     });
+
+  })
 }
+
 module.exports=main
--- test/lambda.test.js
+++ test/lambda.test.js
@@ -1,16 +1,29 @@
 // import {main} from '../resources/lambda.cjs';
+const exp = require('constants');
 const main=require('../resources/lambda.cjs')
 const AWS = require('aws-sdk');
+const { timeout } = require('async');
 const S3 = new AWS.S3();
 
+const findConntract= require('../resources/lambda.cjs')
+const bucketNameis=process.env.BUCKETNAME
+console.log(bucketNameis)
+
+
 describe('testing main',()=>{
-    const consoleSpy=jest.spyOn(global.console,'log');
     test('main function',async()=>{
-        const result =await main()
-        const data = await S3.listObjectsV2({ Bucket: 'cdklambdastack-mylambdastore367fee0e-o3e9kvmjvpgy' }).promise();
-        expect(data).toHaveBeenCalledWith(expect.stringContaining('Slip'))
-        expect(consoleSpy).toHaveBeenCalledWith(expect.stringContaining('Entering Lambda!'))
-        expect(consoleSpy).toHaveBeenCalledWith(expect.stringContaining('after s3'))
-        // expect(result).toBe('something')
+        const consoleSpy=jest.spyOn(global.console,'log');
+        //const result=await main('cdklambdastack-mylambdastore367fee0e-o3e9kvmjvpgy')
+
+        const result = await main(bucketNameis)
+        expect(consoleSpy).toHaveBeenCalledWith(expect.stringContaining('resolved error'));
+        expect(consoleSpy).toHaveBeenCalledWith(expect.stringContaining('Entering Lambda!'));
+        expect(consoleSpy.mock.calls.some(args => args.join(' ').includes('something'))).toBe(true);
+        expect(consoleSpy.mock.calls.some(args => args.join(' ').includes('after deleting'))).toBe(true);
+        expect(consoleSpy.mock.calls.some(args => args.join(' ').includes('error occured'))).toBe(false);
+        consoleSpy.mockRestore();
+
+
+
     })
 })
