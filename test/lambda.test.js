37215e50a93aec605dcbbb4c06c125db5fcc141d
madhura <iammadhura26@gmail.com>
2023-05-23 20:52:53 +0530

    helllo

--- test/lambda.test.js
+++ test/lambda.test.js
@@ -1,8 +1,6 @@
 // import {main} from '../resources/lambda.cjs';
-const exp = require('constants');
 const main=require('../resources/lambda.cjs')
 const AWS = require('aws-sdk');
-const { timeout } = require('async');
 const S3 = new AWS.S3();
 
 const findConntract= require('../resources/lambda.cjs')
