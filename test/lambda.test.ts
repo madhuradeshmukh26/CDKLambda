cee402dc7794d4f44e086bbcafa3ff07230ab6d3
madhura <iammadhura26@gmail.com>
2023-05-29 11:28:37 +0530

    hel

--- test/lambda.test.ts
+++ test/lambda.test.ts
@@ -2,31 +2,11 @@
 const main=require('../resources/lambda.mjs')
 const AWS = require('aws-sdk');
 //const AWS = require('aws-sdk/clients/s3');
-const S3 = require('aws-cdk-lib/aws_s3')
-const miss= require('../resources/lambda.mjs')
-const cdk =require('aws-cdk-lib')
+// const s3 = require('aws-cdk-lib/aws_s3')
+// const miss= require('../resources/lambda.mjs')
+
 
 const findConntract= require('../resources/lambda.mjs')
-
-
-class somm {
-    bul;
-    bulname;
-    buckname
-
-    constructor(scope,id){
-        this.bul="hello"
-
-        this.buckname= cdk.Fn.importValue('sourcebucketappflow')
-        console.log(this.buckname)
-        const bu=S3.fromBucketArn(scope, "bucketexport", this.buckname)
-    }
-}
-
-let res= new somm();
-console.log(`${res.bul}`)
-console.log(`${res.buckname}`)
-
 
 describe('testing main',()=>{
     test('main function',async()=>{
